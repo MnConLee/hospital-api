d738ce37dafe9ca23b36f96d5da3579682f391c3
MnConLee <2360078968@qq.com>
2022-07-31 16:21:13 +0800

    医院模块搭建

---
+++ service/service_hospital/src/main/java/com/lmk/yygh/hospital/ServiceHospitalApplication.java
@@ -0,0 +1,15 @@
+package com.lmk.yygh.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 李明康
+ * @create 2022/7/31 16:06
+ */
+@SpringBootApplication
+public class ServiceHospitalApplication {
+     public static void main(String[] args) {
+           SpringApplication.run(ServiceHospitalApplication.class, args);
+      }
+}
