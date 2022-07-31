45c0d8d845fff5e8823dd182247214fff7c7d55b
MnConLee <2360078968@qq.com>
2022-07-31 21:09:41 +0800

    医院设置实体类建立

---
+++ model/src/main/java/com/lmk/yygh/model/hospital/HospitalSet.java
@@ -0,0 +1,48 @@
+package com.lmk.yygh.model.hospital;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.lmk.yygh.model.base.BaseEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @author Mn
+ * @create 2022/7/31 17:53
+ */
+@Data
+@ApiModel(description = "医院设置")
+@TableName("hospital_set")
+public class HospitalSet extends BaseEntity {
+  private static final long serialVersionUID = 1L;
+
+  @ApiModelProperty(value = "医院名称")
+  @TableField("hosname")
+  private String hosname;
+
+  @ApiModelProperty(value = "医院编号")
+  @TableField("hoscode")
+  private String hoscode;
+
+  @ApiModelProperty(value = "api基础路径")
+  @TableField("api_url")
+  private String apiUrl;
+
+  @ApiModelProperty(value = "签名密钥")
+  @TableField("sign_key")
+  private String signKey;
+
+  @ApiModelProperty(value = "联系人姓名")
+  @TableField("contacts_name")
+  private String contactsName;
+
+  @ApiModelProperty(value = "联系人电话")
+  @TableField("contacts_phone")
+  private String contactsPhone;
+
+  @ApiModelProperty(value = "状态")
+  @TableField("status")
+  private long status;
+
+}
---
+++ service/service_hospital/src/main/java/com/lmk/yygh/hospital/mapper/HospitalSetMapper.java
@@ -0,0 +1,12 @@
+package com.lmk.yygh.hospital.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lmk.yygh.model.hospital.HospitalSet;
+
+/**
+ * @author 李明康
+ * @create 2022/7/31 20:33
+ */
+public interface HospitalSetMapper extends BaseMapper<HospitalSet> {
+}
