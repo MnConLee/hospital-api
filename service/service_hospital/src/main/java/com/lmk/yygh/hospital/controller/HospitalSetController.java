065528f7fdf0ec43f8e2b4c78f3626465b687dcb
MnConLee <2360078968@qq.com>
2022-08-01 13:29:26 +0800

    新增HospSet查询和逻辑删除

--- service/service_hospital/src/main/java/com/lmk/yygh/hospital/controller/HospitalSetController.java
+++ service/service_hospital/src/main/java/com/lmk/yygh/hospital/controller/HospitalSetController.java
@@ -1,8 +1,11 @@
 package com.lmk.yygh.hospital.controller;
 
 import com.lmk.yygh.hospital.service.HospitalSetService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.lmk.yygh.model.hospital.HospitalSet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author 李明康
@@ -11,6 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
 public class HospitalSetController {
+    @Autowired
     private HospitalSetService hospitalSetService;
 
+    /**
+     * 查询医院所有表的基本信息
+     */
+    @GetMapping("findAll")
+    public List<HospitalSet> findAllHospitalSet() {
+        List<HospitalSet> hospitalSetList = hospitalSetService.list();
+        return hospitalSetList;
+    }
+
+    @DeleteMapping("{id}")
+    public boolean removeHospSet(@PathVariable Long id) {
+        boolean flag = hospitalSetService.removeById(id);
+        return flag;
+    }
 }
--- service/service_hospital/src/main/java/com/lmk/yygh/hospital/mapper/HospitalSetMapper.java
+++ service/service_hospital/src/main/java/com/lmk/yygh/hospital/mapper/HospitalSetMapper.java
@@ -3,12 +3,10 @@ package com.lmk.yygh.hospital.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lmk.yygh.model.hospital.HospitalSet;
-import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author 李明康
  * @create 2022/7/31 20:33
  */
-@Mapper
 public interface HospitalSetMapper extends BaseMapper<HospitalSet> {
 }
