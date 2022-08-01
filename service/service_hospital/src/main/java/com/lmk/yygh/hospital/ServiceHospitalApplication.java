5d77b4b9946ef63ed1bea4980ca8acb125472476
MnConLee <2360078968@qq.com>
2022-08-01 14:24:04 +0800

    整合swagger

--- service/service_hospital/src/main/java/com/lmk/yygh/hospital/ServiceHospitalApplication.java
+++ service/service_hospital/src/main/java/com/lmk/yygh/hospital/ServiceHospitalApplication.java
@@ -2,12 +2,14 @@ package com.lmk.yygh.hospital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author 李明康
  * @create 2022/7/31 16:06
  */
 @SpringBootApplication
+@ComponentScan(basePackages = "com.lmk")
 public class ServiceHospitalApplication {
      public static void main(String[] args) {
            SpringApplication.run(ServiceHospitalApplication.class, args);
--- service/service_hospital/src/main/java/com/lmk/yygh/hospital/controller/HospitalSetController.java
+++ service/service_hospital/src/main/java/com/lmk/yygh/hospital/controller/HospitalSetController.java
@@ -2,6 +2,9 @@ package com.lmk.yygh.hospital.controller;
 
 import com.lmk.yygh.hospital.service.HospitalSetService;
 import com.lmk.yygh.model.hospital.HospitalSet;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +14,7 @@ import java.util.List;
  * @author 李明康
  * @create 2022/8/1 11:41
  */
+@Api(tags = "医院设置管理")
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
 public class HospitalSetController {
@@ -20,13 +24,21 @@ public class HospitalSetController {
     /**
      * 查询医院所有表的基本信息
      */
+    @ApiOperation(value = "获取所有医院设置信息")
     @GetMapping("findAll")
     public List<HospitalSet> findAllHospitalSet() {
         List<HospitalSet> hospitalSetList = hospitalSetService.list();
         return hospitalSetList;
     }
 
+    /**
+     * 逻辑删除医院设置
+     * @param id
+     * @return
+     */
+    @ApiOperation(value = "逻辑删除医院设置")
     @DeleteMapping("{id}")
+    @ApiParam(name = "id",value = "医院设置id",required = true)
     public boolean removeHospSet(@PathVariable Long id) {
         boolean flag = hospitalSetService.removeById(id);
         return flag;
