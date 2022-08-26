d17d4f5b943aef9b24e1b911c8aec743814eb17e
MnConLee <2360078968@qq.com>
2022-08-27 01:49:29 +0800

    新增生成二维码

---
+++ service/service_user/src/main/java/com/lmk/yygh/user/api/WeixinApiController.java
@@ -0,0 +1,54 @@
+package com.lmk.yygh.user.api;
+
+import com.lmk.yygh.common.result.Result;
+import com.lmk.yygh.user.service.UserInfoService;
+import com.lmk.yygh.user.utils.ConstantWxPropertiesUtils;
+import com.sun.deploy.net.URLEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 李明康
+ * @create 2022/8/27 0:57
+ */
+//微信操作接口
+@Controller
+@RequestMapping("/api/ucenter/wx")
+public class WeixinApiController {
+
+    @Autowired
+    private UserInfoService userInfoService;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 获取微信登录参数
+     */
+    @GetMapping("getLoginParam")
+    @ResponseBody
+    public Result genQrConnect() {
+        try {
+            Map<String, Object> map = new HashMap<>();
+            map.put("appid", ConstantWxPropertiesUtils.WX_OPEN_APP_ID);
+            map.put("scope","snsapi_login");
+            String wxOpenRedirectUrl = ConstantWxPropertiesUtils.WX_OPEN_REDIRECT_URL;
+            wxOpenRedirectUrl = URLEncoder.encode(wxOpenRedirectUrl, "utf-8");
+            map.put("redirect_uri",wxOpenRedirectUrl);
+            map.put("state",System.currentTimeMillis()+"");
+            return Result.ok(map);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
